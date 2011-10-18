11c202acc38d9b04dfe71fa7df81bda07bbb891d
Brett Morgan <brett.morgan@gmail.com>
2011-10-18 15:31:39 +1100

    Now with JS that passes JSlint

--- static/cah.js
+++ static/cah.js
@@ -22,4 +22,105 @@
  * @author Nick Johnson (Google)
  */
 
+/*jslint browser: true, unparam: true, sloppy: true, nomen: true, maxlen: 100, indent: 2 */
 
+/**
+ * Shared state of the app.
+ * @type {Object.<!string, !string>}
+ * @private
+ */
+var state_ = null;
+
+/**
+ * Describes the shared state of the object.
+ * @type {Object.<!string, Object.<!string, *>>}
+ * @private
+ */
+var metadata_ = null;
+
+/**
+ * A list of the participants.
+ * @type {Array.<gapi.hangout.Participant>}
+ * @private
+ */
+var participants_ = null;
+
+/**
+ * Create required DOM elements and listeners.
+ */
+function prepareAppDOM() {
+  // TODO(brettmorgan): do something
+}
+
+/**
+ * Renders the app.
+ */
+function render() {
+  // TODO(brettmorgan): do something
+}
+
+/**
+ * Syncs local copies of shared state with those on the server and renders the
+ *     app to reflect the changes.
+ * @param {!Array.<Object.<!string, *>>} add Entries added to the shared state.
+ * @param {!Array.<!string>} remove Entries removed from the shared state.
+ * @param {!Object.<!string, !string>} state The shared state.
+ * @param {!Object.<!string, Object.<!string, *>>} metadata Data describing the
+ *     shared state.
+ */
+function onStateChanged(add, remove, state, metadata) {
+  state_ = state;
+  metadata_ = metadata;
+  render();
+}
+
+/**
+ * Syncs local copy of the participants list with that on the server and renders
+ *     the app to reflect the changes.
+ * @param {!Array.<gapi.hangout.Participant>} participants The new list of
+ *     participants.
+ */
+function onParticipantsChanged(participants) {
+  participants_ = participants;
+  render();
+}
+
+(function () {
+  if (gapi && gapi.hangout) {
+
+    var initHangout = function () {
+      var initState, initMetadata, added, key, removed, initParticipants;
+
+      prepareAppDOM();
+
+      gapi.hangout.data.addStateChangeListener(onStateChanged);
+      gapi.hangout.addParticipantsListener(onParticipantsChanged);
+
+      if (!state_) {
+        initState = gapi.hangout.data.getState();
+        initMetadata = gapi.hangout.data.getStateMetadata();
+        added = [];
+
+        for (key in initMetadata) {
+          if (initMetadata.hasOwnProperty(key)) {
+            added.push(initMetadata[key]);
+          }
+        }
+        removed = [];
+        if (initState && initMetadata) {
+          onStateChanged(added, removed, initState, initMetadata);
+        }
+      }
+      if (!participants_) {
+        initParticipants = gapi.hangout.getParticipants();
+        if (initParticipants) {
+          onParticipantsChanged(initParticipants);
+        }
+      }
+
+      gapi.hangout.removeApiReadyListener(initHangout);
+    };
+
+    gapi.hangout.addApiReadyListener(initHangout);
+  }
+}());
