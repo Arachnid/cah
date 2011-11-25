7028267b72cc7c7b2510374ca091a817b770aab0
Nick Johnson <arachnid@notdot.net>
2011-11-25 13:38:23 +1100

    Updated models with more useful fields

--- models.py
+++ models.py
@@ -1,15 +1,34 @@
 from google.appengine.api import channel
 from ndb import model
 
+GAME_STATES = ['new', 'start_round', 'voting', 'scores']
+
 class Game(model.Model):
+
+  state = model.StringProperty(choices=STATES)
+  question_deck = model.IntegerProperty(repeated=True)
+  answer_deck = model.IntegerProperty(repeated=True)
+  current_question = model.IntegerProperty()
+  is_paused = model.BooleanProperty(required=True, default=False)
+  start_time = model.DateTimeProperty(required=True, auto_now_add=True)
+  end_time = model.DateTimeProperty()
+
   @property
   def hangout_id(self):
     return self.key().name()
-
+  
 
 class Participant(model.Model):
   # Child entity of the Game in which they are participating
   
+  channel_id = model.StringProperty(indexed=False)
+  channel_token = model.StringProperty(indexed=False)
+  playing = model.BooleanProperty(required=True, default=True)
+  score = model.IntegerProperty(required=True, default=0)
+  cards = model.IntegerProperty(repeated=True)
+  selected_card = model.IntegerProperty()
+  vote = model.KeyProperty()
+
   @property
   def plus_id(self):
     """The user's Google+ ID."""
@@ -30,7 +49,3 @@ class Participant(model.Model):
       participant.put()
       return participant
     return model.transaction(_tx)
-
-  channel_id = model.StringProperty(indexed=False)
-  channel_token = model.StringProperty(indexed=False)
-  playing = model.BooleanProperty(default=True)
